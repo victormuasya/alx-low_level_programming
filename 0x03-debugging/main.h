f35f1aa86ebbf176dbfbe53a9e2131b8511f8a8b
MohamedTwahir <mohamedtwahir977@gmail.com>
2023-02-22 12:49:34 +0300

    debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+#ifndef A2DB6B89_FD6F_4E79_885C_1D3861186258
+#define A2DB6B89_FD6F_4E79_885C_1D3861186258
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
+
+#endif /* A2DB6B89_FD6F_4E79_885C_1D3861186258 */
