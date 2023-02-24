47de642b127ff8988641571c99e51b56a58973ed
MohamedTwahir <mohamedtwahir977@gmail.com>
2023-02-24 07:54:16 +0300

    more nested loops

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef BD326B74_0554_4906_8AD7_0F21CC63A7EE
+#define BD326B74_0554_4906_8AD7_0F21CC63A7EE
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
+
+#endif /* BD326B74_0554_4906_8AD7_0F21CC63A7EE */
