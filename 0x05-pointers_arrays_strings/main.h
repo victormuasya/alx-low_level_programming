75779e52ade316f3928f144b799b6e0a7fa88b77
MohamedTwahir <mohamedtwahir977@gmail.com>
2023-02-27 13:22:30 +0300

    arr & pointers/strings

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef B9C0FB76_B59D_49C2_95FD_825984D16951
+#define B9C0FB76_B59D_49C2_95FD_825984D16951
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif /* B9C0FB76_B59D_49C2_95FD_825984D16951 */
