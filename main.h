6c6ad597ade9b71fdaa5b000cad86744c46b009c
MohamedTwahir <mohamedtwahir977@gmail.com>
2023-03-07 23:23:56 +0300

    pointers,arr

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef AAC51BFD_F85E_41F9_8C64_1C7F4CDADE65
+#define AAC51BFD_F85E_41F9_8C64_1C7F4CDADE65
+int _putchar(char);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif /* AAC51BFD_F85E_41F9_8C64_1C7F4CDADE65 */
