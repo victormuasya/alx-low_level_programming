7b6d7e1c889f518637c2ee687426cffa2d86c01d
victormuasya <vicwambua251@gmail.com>
2023-03-17 12:56:59 -0700

    reallocation

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,22 @@
+#ifndef FEC01944_2D76_4F7E_843A_6AE039FF81E7
+#define FEC01944_2D76_4F7E_843A_6AE039FF81E7
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0B-more_malloc_free directory.
+ */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
+
+#endif /* FEC01944_2D76_4F7E_843A_6AE039FF81E7 */
