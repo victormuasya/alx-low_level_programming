c680f4b827baa9b4def0ac7fd77ab062659a13ad
victormuasya <vicwambua251@gmail.com>
2023-04-04 22:22:44 +0300

    free_listint

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+
+#include "lists.h"
+
+/** file: 4-free_listint.c */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
