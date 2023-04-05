ce389c9996080815eb6b1b85ccb5925122f54d39
victormuasya <vicwambua251@gmail.com>
2023-04-05 18:02:23 +0300

    lists

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,31 @@
+/*File: 103-find_loop.c*/
+
+#include "lists.h"
+
+listint_t *find_listint_loop(listint_t *head){
+	listint_t *tortoise, *hare;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare){
+		if (tortoise == hare){
+			tortoise = head;
+
+			while (tortoise != hare){
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			return (tortoise);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+
+	return (NULL);
+}
